c3d340c800585b8c82ae8d048394ee0e4f6da601
M-Orxan <orxanm385@gmail.com>
2024-11-27 22:54:23 +0400

    connected to sql

---
+++ football-tournament-maker-mvc/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using football_tournament_maker_mvc.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace football_tournament_maker_mvc.Data
+{
+    public class AppDbContext:IdentityDbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+    }
+}
---
+++ football-tournament-maker-mvc/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace football_tournament_maker_mvc.Models
+{
+    public class AppUser:IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        public string Email { get; set; }
+        
+    }
+}
